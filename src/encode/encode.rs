75a557b40533ee91179302cd69afd49e5a4bb805
jmaczan <jedrzejpawel@maczan.pl>
2022-02-18 19:43:55 +0100

    Encode - first version

--- src/encode/encode.rs
+++ src/encode/encode.rs
@@ -1,12 +1,14 @@
-pub fn encode(clear_text: &str) {
+
+pub fn encode(clear_text: &str) -> String {
     let binary = to_binary(clear_text);
     let binary_groups = to_binary_groups(binary);
     let prefixed_binary_groups = prefix_with_zeros(binary_groups);
-    
-    for element in prefixed_binary_groups {
-        println!("{}", element)
-    }
+    let ascii_decimals = to_ascii_decimal(prefixed_binary_groups);
+    let encoded_text = to_ascii_string(ascii_decimals);
+
+    encoded_text
 }
+
 
 fn to_binary(text: &str) -> String {
     let name = text.to_string();
@@ -38,8 +40,27 @@ fn prefix_with_zeros(binary_groups: Vec<String>) -> Vec<String> {
 
     for character in binary_groups {
         prefixed_binary_groups.push((&format!("00{}", character)).to_string());
-        // println!("{}", &format!("00{}", character))
     }
 
     prefixed_binary_groups
+}
+
+fn to_ascii_decimal(binary_groups: Vec<String>) -> Vec<String> {
+    let mut ascii_decimals: Vec<String> = Vec::new();
+
+    for element in binary_groups {
+        ascii_decimals.push(u8::from_str_radix(&element, 2).unwrap().to_string());
+    }
+
+    ascii_decimals
+}
+
+fn to_ascii_string(ascii_decimals: Vec<String>) -> String {
+    let mut ascii_string = "".to_string();
+
+    for decimal in ascii_decimals {
+        ascii_string += &"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/".chars().nth(decimal.parse::<usize>().unwrap()).unwrap().to_string();
+    }
+
+    ascii_string.to_string()
 }
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ pub mod encode;
 
 fn main() {
     let text_to_encode: &str = "ABC";
-    println!("{}", text_to_encode);
-    encode::encode::encode(text_to_encode);
+    println!("Clear text: {}", text_to_encode);
+    println!("Encoded text: {}", encode::encode::encode(text_to_encode));
 }
