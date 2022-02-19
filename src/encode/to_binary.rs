f217f39539c1027b445695b5d4c60f24434b74a9
jmaczan <jedrzejpawel@maczan.pl>
2022-02-19 12:34:04 +0100

    Refactor to_binary to more functional form

--- src/encode/to_binary.rs
+++ src/encode/to_binary.rs
@@ -1,27 +1,28 @@
 pub fn to_binary(text: String) -> String {
-    let name = text;
-    let mut name_in_binary = "".to_string();
+    text.clone()
+        .into_bytes()
+        .into_iter()
+        .map(|character| {
+            let binary_character = format!("0{:b}", character);
 
-    for character in name.clone().into_bytes() {
-        let mut binary_character = format!("0{:b}", character);
+            let binary_length = binary_character.len();
 
-        let binary_length = binary_character.len();
+            if binary_length != 8 {
+                let mut zeros = "".to_string();
 
-        binary_character = if binary_length != 8 {
-            let mut zeros = "".to_string();
+                for _ in 0..(8 - binary_length) {
+                    zeros = format!("{}{}", &zeros, "0");
+                }
 
-            for _ in 0..(8 - binary_length) {
-                zeros = format!("{}{}", &zeros, "0");
+                format!("{}{}", zeros, binary_character)
+            } else {
+                binary_character
             }
-
-            format!("{}{}", zeros, binary_character)
-        } else {
-            binary_character
-        };
-
-        println!("binary_character: {}", binary_character);
-        name_in_binary += &binary_character;
-    }
-    println!("to_binary: {}", name_in_binary);
-    name_in_binary
+        })
+        .collect::<Vec<String>>()
+        .into_iter()
+        .reduce(|name_in_binary, binary_character| {
+            format!("{}{}", name_in_binary, binary_character)
+        })
+        .unwrap()
 }
