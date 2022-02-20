a1cfdf83694621afe213e022a6a3390274fd23b4
jmaczan <jedrzejpawel@maczan.pl>
2022-02-20 15:56:09 +0100

    Rewrite prefix_with_zeros in more functional way

--- src/encode/prefix_with_zeros.rs
+++ src/encode/prefix_with_zeros.rs
@@ -1,15 +1,8 @@
 pub fn prefix_all_with_zeros(strings_to_prefix: Vec<String>, desired_length: usize) -> Vec<String> {
-    let mut prefixed_strings: Vec<String> = Vec::new();
-
-    for string_to_prefix in strings_to_prefix {
-        println!(
-            "prefixed with zeros: {}",
-            (&format!("00{}", string_to_prefix)).to_string()
-        );
-        prefixed_strings.push(prefix_with_zeros(string_to_prefix, desired_length));
-    }
-
-    prefixed_strings
+    strings_to_prefix
+        .into_iter()
+        .map(|string| prefix_with_zeros(string, desired_length))
+        .collect::<Vec<String>>()
 }
 
 pub fn prefix_with_zeros(string_to_prefix: String, desired_length: usize) -> String {
