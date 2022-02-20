7a3bdadfc92fbba7eec2aa72c240fa8f3edefc3c
jmaczan <jedrzejpawel@maczan.pl>
2022-02-20 15:51:40 +0100

    Resolve issue with not parsing first character in to_ascii_string using reduce - the problem was that reduce takes first element as an accumulator initial value

--- src/encode/to_ascii_string.rs
+++ src/encode/to_ascii_string.rs
@@ -1,13 +1,19 @@
 pub fn to_ascii_string(ascii_decimals: Vec<String>) -> String {
-    let mut ascii_string = "".to_string();
-
-    for decimal in ascii_decimals {
-        ascii_string += &"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
-            .chars()
-            .nth(decimal.parse::<usize>().unwrap())
-            .unwrap()
-            .to_string();
-    }
-
-    ascii_string.to_string()
+    let all_ascii_characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    let mut ascii_decimals_copy: Vec<String> = vec!["".to_string()];
+    ascii_decimals_copy.extend(ascii_decimals);
+    ascii_decimals_copy
+        .into_iter()
+        .reduce(|ascii_strings, ascii_string| {
+            format!(
+                "{}{}",
+                ascii_strings,
+                &all_ascii_characters
+                    .chars()
+                    .nth(ascii_string.parse::<usize>().unwrap())
+                    .unwrap()
+                    .to_string()
+            )
+        })
+        .unwrap()
 }
