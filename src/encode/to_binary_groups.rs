a16aff6bc4e7aa32d31e1e27be261b6281a177ab
jmaczan <jedrzejpawel@maczan.pl>
2022-02-20 17:19:06 +0100

    Refactor functions in to_binary_groups

--- src/encode/to_binary_groups.rs
+++ src/encode/to_binary_groups.rs
@@ -7,27 +7,43 @@ pub fn to_binary_groups(binary_string: String) -> (Vec<String>, usize) {
     let mut last_byte_length: usize = constants::BINARY_GROUPS_INITIAL_LENGTH;
 
     while binary_string_copy.chars().count() > 0 {
-        let group_length = if binary_string_copy.len() > constants::BINARY_GROUPS_INITIAL_LENGTH {
-            constants::BINARY_GROUPS_INITIAL_LENGTH
-        } else {
-            binary_string_copy.len()
-        };
-        let group = if group_length != constants::BINARY_GROUPS_INITIAL_LENGTH {
-            let mut zeros = "".to_string();
-            last_byte_length = group_length;
+        let group_length = get_group_length(binary_string_copy);
 
-            for _ in 0..(constants::BINARY_GROUPS_INITIAL_LENGTH - group_length) {
-                zeros = format!("{}{}", &zeros, constants::ZERO_STRING);
-            }
+        let (group, possible_last_byte_length) =
+            create_binary_group(binary_string_copy, group_length);
 
-            format!("{}{}", &binary_string_copy[..group_length], zeros).to_string()
-        } else {
-            binary_string_copy[..group_length].to_string()
-        };
-        println!("binary group: {}", group);
+        if possible_last_byte_length.is_some() {
+            last_byte_length = possible_last_byte_length.unwrap()
+        }
+
         binary_string_copy = &binary_string_copy[group_length..];
         binary_groups.push(group.to_string());
     }
 
     (binary_groups, last_byte_length)
+}
+
+fn get_group_length(binary_string: &str) -> usize {
+    if binary_string.len() > constants::BINARY_GROUPS_INITIAL_LENGTH {
+        constants::BINARY_GROUPS_INITIAL_LENGTH
+    } else {
+        binary_string.len()
+    }
+}
+
+fn create_binary_group(binary_string: &str, group_length: usize) -> (String, Option<usize>) {
+    if group_length != constants::BINARY_GROUPS_INITIAL_LENGTH {
+        let mut zeros = "".to_string();
+
+        for _ in 0..(constants::BINARY_GROUPS_INITIAL_LENGTH - group_length) {
+            zeros = format!("{}{}", &zeros, constants::ZERO_STRING);
+        }
+
+        (
+            format!("{}{}", &binary_string[..group_length], zeros).to_string(),
+            Some(group_length),
+        )
+    } else {
+        (binary_string[..group_length].to_string(), None)
+    }
 }
