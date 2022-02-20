2f722d8469f1ccad6374faa9006afd715923ee8f
jmaczan <jedrzejpawel@maczan.pl>
2022-02-20 15:54:24 +0100

    Rewrite to_ascii_decimal in more functional way

--- src/encode/to_ascii_decimal.rs
+++ src/encode/to_ascii_decimal.rs
@@ -1,11 +1,6 @@
 pub fn to_ascii_decimal(binary_groups: Vec<String>) -> Vec<String> {
-    let mut ascii_decimals: Vec<String> = Vec::new();
-
-    for element in binary_groups {
-        let ascii_decimal: String = u8::from_str_radix(&element, 2).unwrap().to_string();
-        println!("ascii decimal: {}", ascii_decimal);
-        ascii_decimals.push(ascii_decimal);
-    }
-
-    ascii_decimals
+    binary_groups
+        .into_iter()
+        .map(|decimal| u8::from_str_radix(&decimal, 2).unwrap().to_string())
+        .collect::<Vec<String>>()
 }
